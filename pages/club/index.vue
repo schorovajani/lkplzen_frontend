db9c0f2c50a470978ab7885f321d3935e7fd5431
Jana Schořová <schorjan@fit.cvut.cz>
2022-04-21 17:43:18 +0200

    Add tables and menu

--- pages/club/index.vue
+++ pages/club/index.vue
@@ -63,18 +63,187 @@
             nesciunt. Fusce nibh. Mauris metus. Quisque porta. Sed ac dolor sit
             amet purus malesuada congue.
           </p>
-          <nuxt-link to="/club/start" tag="button" class="page-button"
-            >Jak začít střílet?</nuxt-link
-          >
+          <nuxt-link to="/start" tag="button" class="page-button">
+            Jak začít střílet?
+          </nuxt-link>
         </article>
+      </div>
+    </section>
+    <section class="page-section page-second">
+      <div class="page-layout page-cols">
+        <div class="page-column">
+          <h3>Lidé v klubu</h3>
+          <div class="people">
+            <div class="people-menu">
+              <button
+                @click="changeTable('members')"
+                class="people-button"
+                :class="{ active: activeTable === 'members' }"
+              >
+                Členové
+              </button>
+              <button
+                @click="changeTable('coaches')"
+                class="people-button"
+                :class="{ active: activeTable === 'coaches' }"
+              >
+                Trenéři
+              </button>
+              <button
+                @click="changeTable('committee')"
+                class="people-button"
+                :class="{ active: activeTable === 'committee' }"
+              >
+                Předsednictvo
+              </button>
+              <button
+                @click="changeTable('league')"
+                class="people-button"
+                :class="{ active: activeTable === 'league' }"
+              >
+                Ligová družstva
+              </button>
+            </div>
+            <div
+              id="members"
+              class="people-list"
+              :class="{ hide: activeTable !== 'members' }"
+            >
+              <table>
+                <thead>
+                  <tr>
+                    <th>Příjmení a jméno</th>
+                    <th>Věková kategorie</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="member in members" :key="member.id">
+                    <td>{{ member.name }}</td>
+                    <td>{{ member.ageCategory }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div
+              id="coaches"
+              class="people-list"
+              :class="{ hide: activeTable !== 'coaches' }"
+            >
+              <table>
+                <thead>
+                  <tr>
+                    <th>Příjmení a jméno</th>
+                    <th>Vzdělání</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="coach in choaches" :key="coach.id">
+                    <td>
+                      <nuxt-link to="/club/people/coach.slug">
+                        {{ coach.name }}
+                      </nuxt-link>
+                    </td>
+                    <td>{{ coach.education }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div
+              id="committee"
+              class="people-list"
+              :class="{ hide: activeTable !== 'committee' }"
+            >
+              <table>
+                <thead>
+                  <tr>
+                    <th>Příjmení a jméno</th>
+                    <th>Role</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="person in committee" :key="person.id">
+                    <td>
+                      <nuxt-link to="/club/people/person.slug">
+                        {{ person.name }}
+                      </nuxt-link>
+                    </td>
+                    <td>{{ person.committeeRole }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div
+              id="league"
+              class="people-list"
+              :class="{ hide: activeTable !== 'league' }"
+            >
+              <h4>Liga mužů</h4>
+              <table>
+                <tbody>
+                  <tr v-for="archer in menLeague" :key="archer.id">
+                    <td>
+                      <nuxt-link to="/club/people/archer.slug">
+                        {{ archer.name }}
+                      </nuxt-link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <h4>Liga žen</h4>
+              <table>
+                <tbody>
+                  <tr v-for="archer in womenLeague" :key="archer.id">
+                    <td>
+                      <nuxt-link to="/club/people/archer.slug">
+                        {{ archer.name }}
+                      </nuxt-link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <h4>Liga mládeže</h4>
+              <table>
+                <tbody>
+                  <tr v-for="archer in youthLeague" :key="archer.id">
+                    <td>
+                      <nuxt-link to="/club/people/archer.slug">
+                        {{ archer.name }}
+                      </nuxt-link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="page-column"></div>
       </div>
     </section>
   </main>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      activeTable: 'members',
+    }
+  },
+  methods: {
+    changeTable(type) {
+      this.activeTable = type
+    },
+  },
+}
+</script>
+
 <style lang="scss" scoped>
 .page-section {
   padding: 6rem 0;
+}
+
+.page-second {
+  background-color: $grey;
 }
 
 .page-layout {
@@ -100,10 +269,58 @@
   p {
     margin: 1rem 0;
   }
+}
 
-  button {
-    margin: 2rem 0;
-    align-self: flex-end;
+.page-button {
+  margin: 2rem 0;
+  align-self: flex-end;
+}
+
+.people-menu {
+  margin: 0.8rem 0;
+}
+.people-button {
+  background: none;
+  border: none;
+
+  color: $white;
+  font-size: 1rem;
+  margin: 0.2rem 0rem;
+  padding: 0.5rem 0.8rem;
+}
+
+.active {
+  color: $black;
+  background-color: $primary;
+  border-radius: 20px;
+}
+
+.people-list {
+  table {
+    width: 100%;
+    background-color: $white;
+
+    thead {
+      border-bottom: 1px solid $black;
+
+      tr th {
+        text-align: left;
+        padding: 0.8rem 1rem;
+        font-weight: 600;
+      }
+    }
+    tbody {
+      tr td {
+        padding: 0.6rem 1rem;
+      }
+      tr:nth-child(odd) {
+        background: #dddddd;
+      }
+    }
   }
+}
+
+.hide {
+  display: none;
 }
 </style>

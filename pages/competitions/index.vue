29a9c72eb8868f0f3a69219cf7607fa8dfe39615
Jana Schořová <schorjan@fit.cvut.cz>
2022-05-11 09:38:53 +0200

    Add structure

--- pages/competitions/index.vue
+++ pages/competitions/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <PagesHead title="Závody" image="club.jpg" />
+  <main>
+    <PagesHead title="Závody" image="club.jpg" />
+    <section id="posts" class="page-section">
+      <div class="page-layout">
+        <h3>Aktuality</h3>
+        <p>zde budou aktuality</p>
+      </div>
+    </section>
+    <section id="success" class="page-section">
+      <div class="page-layout">
+        <h3>Naše úspěchy</h3>
+        <p>zde budou naše úspěchy</p>
+      </div>
+    </section>
+    <section id="records" class="page-section">
+      <div class="page-layout">
+        <h3>Klubové rekordy</h3>
+        <p>zde budou klubové rekordy</p>
+      </div>
+    </section>
+  </main>
 </template>
+
+<style lang="scss" scoped>
+#success {
+  background-color: $secondary;
+  color: $white;
+}
+</style>
--- pages/trainings/index.vue
+++ pages/trainings/index.vue
@@ -1,3 +1,64 @@
 <template>
-  <PagesHead title="Tréninky" image="club.jpg" />
+  <main>
+    <PagesHead title="Tréninky" image="trainings.jpg" />
+    <section class="page-section training-info">
+      <p>doplňující Informace</p>
+    </section>
+    <section id="summer" class="page-section">
+      <div class="page-layout">
+        <div class="page-cols">
+          <div class="page-column">
+            <h3>Letní měsíce</h3>
+            <p>text a mapa</p>
+          </div>
+          <div class="page-column">
+            <p>tabulka s časy tréninků</p>
+          </div>
+        </div>
+        <div class="other-photos">
+          <p>zde budou doplňující foto a video</p>
+        </div>
+      </div>
+    </section>
+    <section id="winter" class="page-section">
+      <div class="page-layout">
+        <div class="page-cols">
+          <div class="page-column">
+            <p>tabulka s časy tréninků</p>
+          </div>
+          <div class="page-column">
+            <h3>Zimní měsíce</h3>
+            <p>text a mapa</p>
+          </div>
+        </div>
+        <div class="other-photos">
+          <p>zde budou doplňující foto a video</p>
+        </div>
+      </div>
+    </section>
+    <section id="camp" class="page-section">
+      <div class="page-cols page-layout">
+        <div class="page-column">
+          <h3>Soustředění</h3>
+          <p>text</p>
+        </div>
+        <div class="page-column">
+          <p>foto</p>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
+
+<style lang="scss" scoped>
+#winter {
+  background-color: $secondary;
+  color: $white;
+}
+
+.training-info {
+  p {
+    text-align: center;
+  }
+}
+</style>

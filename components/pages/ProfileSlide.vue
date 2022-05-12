1b4fda1265c0dc04adec192294d66219c2391e1f
Jana Schořová <schorjan@fit.cvut.cz>
2022-05-12 11:41:57 +0200

    Add profiles to club page

---
+++ components/pages/ProfileSlide.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="slide-container">
+    <vueper-slides
+      autoplay
+      class="no-shadow"
+      :bullets="false"
+      fixed-height="850px"
+    >
+      <vueper-slide v-for="(profile, i) in profiles" :key="i">
+        <template v-slot:content>
+          <Profile :profile="profile.attributes" />
+        </template>
+      </vueper-slide>
+    </vueper-slides>
+  </div>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
+export default {
+  components: { VueperSlides, VueperSlide },
+  props: {
+    profiles: Array,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.slide-container {
+  // width: 90vw;
+  // height: 100%;
+  // margin-top: 3rem;
+  display: none;
+}
+
+@media (min-width: 992px) {
+  .slide-container {
+    display: block;
+    margin-top: 8rem;
+    width: 80%;
+  }
+}
+</style>
